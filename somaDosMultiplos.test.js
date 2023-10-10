9f72c9cae87287d9ef56f89b9a7ab0385105218b
Pati Nogueira <pati.p.nogueira@gmail.com>
2023-10-10 15:08:21 -0300

    desafios v.2

--- somaDosMultiplos.test.js
+++
@@ -1,7 +0,0 @@
-const {somaMultiplos} = require('./somaDosMultiplos')
-
-describe('função que retorne a soma de todos os múltiplos de 5 ou 7 abaixo de 1000', () => {
-    it('deve retornar a soma de todos os múltiplos de 5 ou 7 abaixo de 1000', () => {
-        expect(somaMultiplos()).toBe(156361)
-    });
-});
